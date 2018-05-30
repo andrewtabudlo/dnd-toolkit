ae7168a9321a69cd2faa47460773118101dfead9
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-05-30 11:31:44 -0400

    create initial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Campaign.create(name: "Journey Through Imril", user_id: 1)
+
+Character.create(name: "Magnus Thorhammer", hp: 21, initiative: 7, campaign_id: 1)
+Character.create(name: "Pierce Brosnyan", hp: 18, initiative: 15, campaign_id: 1)
+Character.create(name: "Ellerey Dawn", hp: 15, initiative: 8, campaign_id: 1)
+Character.create(name: "Atheina", hp: 17, initiative: 11, campaign_id: 1)
+Character.create(name: "Rowan", hp: 22, initiative: 5, campaign_id: 1)
+Character.create(name: "Kaiser Nosay", hp: 20, initiative: 12, campaign_id: 1)
